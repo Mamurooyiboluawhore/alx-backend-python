75ed1f4819048e9f4b2a2d2d340d0ba23165f05d
Mamuro oyiboluawhore <missmarvelous2016@gmail.com>
2023-07-11 18:41:45 +0000

    imported generator

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -5,9 +5,9 @@ import random
 from typing import Generator
 
 
-async def async_generator() -> Generator[float]:
+async def async_generator() -> Generator[float, None, None]:
     ''' a function that genrate random float'''
     for i in range(10):
         await asyncio.sleep(1)
-        i = random.uniform(1, 10)
-        yield i
+        m = random.uniform(1, 10)
+        yield m
