b03c429f11753b832785ef39bd50f9abbfc3c86f
Mamuro oyiboluawhore <missmarvelous2016@gmail.com>
2023-07-11 18:13:26 +0000

    added the return value

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 import asyncio
-
+from typing import List
 async_generator = __import__('0-async_generator').async_generator
 '''  Async Comprehensions'''
 
 
-async def async_comprehension():
+async def async_comprehension() -> List[float]:
     ''' a function that collects random numbers using comprehension'''
     comp = [number async for number in async_generator()]
     return comp
