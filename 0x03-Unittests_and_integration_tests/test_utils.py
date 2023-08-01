44d9abbe39cbb30b35e18fb1983bcf89723669c5
Mamuro oyiboluawhore <missmarvelous2016@gmail.com>
2023-08-01 00:59:06 +0000

    implement assertRaises

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-from typing import Mapping, Tuple, Union
+from typing import Mapping, Tuple, Union, Any, Type
 import unittest
 from utils import access_nested_map
 from parameterized import parameterized
@@ -12,7 +12,6 @@ class TestAccessNestedMap(unittest.TestCase):
         ({"a": {"b": 2}}, ("a",), {"b": 2}),
         ({"a": {"b": 2}}, ("a", "b"), 2),
     ])
-
     def test_access_nested_map(
             self,
             nested_map: Mapping,
@@ -21,3 +20,17 @@ class TestAccessNestedMap(unittest.TestCase):
             ) -> None:
         ''' Parameterize a unit test'''
         self.assertEqual(access_nested_map(nested_map, path), expected)
+
+    @parameterized.expand([
+        ({}, ("a",), KeyError),
+        ({"a": 1}, ("a", "b"), KeyError),
+        ])
+    def test_access_nested_map_exception(
+            self,
+            nested_map: Mapping[Any, Any],
+            path: Tuple[str],
+            expected: Type[Exception]
+            ) -> None:
+        ''' Parameterize a unit test'''
+        with self.assertRaises(expected):
+            access_nested_map(nested_map, path)
