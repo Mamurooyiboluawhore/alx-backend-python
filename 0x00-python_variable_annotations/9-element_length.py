d1adfc1b0b431e9415805082357afd718a1af881
Mamuro oyiboluawhore <missmarvelous2016@gmail.com>
2023-08-19 14:56:37 +0000

    duck-typing

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
+''' duck type'''
 from typing import List, Tuple, Iterable, Sequence
 
 
 def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+    ''' duck type '''
     return [(i, len(i)) for i in lst]
